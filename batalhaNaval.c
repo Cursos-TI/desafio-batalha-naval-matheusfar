0deaa99417b315284b02a5c8ab0363d0d11726ee
matheusfar <matheusf.ar@hotmail.com>
2025-11-07 22:14:09 +0000

    Nivel Aventureiro - Completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -37,18 +37,19 @@ int main() {
     //Declaração das variáveis que vão definir as coordenadas inicias do navio 1 
 
     int linha_navio1 = 2;
-    int coluna_navio1 = 5;
-    int coluna_final = 8;
+    int coluna_navio1 = 4;
+    int final_navio1 = 7;
 
+    //Condição para verificar se o navio esta dentro dos limites do tabuleiro
 
-    if((linha_navio1 >= 0 && linha_navio1 <= 10) && (coluna_navio1 >= 0 && coluna_final <= 10)){ // Verificação se o navio esta nos limites do tabuleiro
+    if((linha_navio1 >= 0 && linha_navio1 <= 10) && (coluna_navio1 >= 0 && final_navio1 <= 10)){ 
        
         
-        for(int i = 0; i < 10; i++){ // Loop para percorrer todo o tabuleiro
+        for(int i = 0; i < 10; i++){ // Loop para percorrer todas as linhas
 
-            for(int j = 0; j < 10; j++){
+            for(int j = 0; j < 10; j++){ // Loop para percorrer todas as colunas
 
-                if(i == linha_navio1 && j >= coluna_navio1 && j < coluna_final){ // Coordenadas do navio 1 
+                if(i == linha_navio1 && j >= coluna_navio1 && j < final_navio1){ // Condição para verificar a coordenadas do navio 1
                         
                     if (tabuleiro[linha_navio1][j] != 0) { // Verifica se ja tem um navio na posição. Se tiver , exibi a mensagem e encerra o programa 
                         printf("Erro ! Navio encontrado na posição [%d][%d] do tabuleiro.", linha_navio1 , j);
@@ -57,9 +58,13 @@ int main() {
 
                     }else{
 
+                        if(posicao < 3) {  //Condição para garantir que o tamanho do navio vai ser respeitado
+
                         tabuleiro[linha_navio1][j] = navio1[posicao]; // Copiando valor do vetor para a posição do navio no tabuleiro
-                        
+                    
                         posicao++;
+
+                        }
                     }
                         
                     }
@@ -82,25 +87,30 @@ int main() {
 
     int linha_navio2 = 5;
     int coluna_navio2 = 7;
-    int coluna_final2 = 8;
+    int final_navio2 = 8;
 
-    if((linha_navio2 >= 0 && linha_navio2 <= 10) && (coluna_navio2 >= 0 && coluna_final2 <= 10)){ // Verificaçao se o navio esta no limite do tabuleiro
+    //Condição para verificar se o navio esta dentro dos limites do tabuleiro
+    if((linha_navio2 >= 0 && linha_navio2 <= 10) && (coluna_navio2 >= 0 && final_navio2 <= 10)){ 
 
-        for(int i = 0; i < 10; i++){ // loop para percorrer todo tabuleiro
+        for(int i = 0; i < 10; i++){ // Loop para percorrer todas as linhas
 
-            for(int j = 0; j < 10; j++){
+            for(int j = 0; j < 10; j++){ // Loop para percorrer todas as colunas
 
-                if(j == coluna_navio2 && i >= linha_navio2 && i < coluna_final2){
+                if(j == coluna_navio2 && i >= linha_navio2 && i < final_navio2){ // Condição para verificar a coordenadas do navio 2
 
                     if(tabuleiro[i][coluna_navio2] != 0 ){ // Verifica se ja tem um navio na posição. Se tiver exibi a mensagem e encerra o programa.
                         printf("Erro ! Navio encontrado na posição [%d][%d] do tabuleiro.\n" , i , coluna_navio2);
                         return 1;
 
                     }else{
-                        // explicar o pq nao coloquei a verificação de if posiçao < 3
+
+                        if(posicao < 3) {  // Condição para garantir que o tamanho do navio vai ser respeitado.
+                        
                          tabuleiro[i][coluna_navio2] = navio2[posicao]; // Copiando valor do vetor para a posição do navio no tabuleiro
 
                          posicao++;
+
+                        }
                     }
                 }
             }
@@ -110,6 +120,105 @@ int main() {
         printf("Erro ! Navio 2 ultrapassou os limites do tabuleiro.\n");
         return 1;
     }
+
+
+    //6. Posicionando navio 3 na diagonal princial
+
+    int navio3[3] = {3 , 3, 3};
+
+    //Declaração das variáveis que vão definir as coordenadas inicias do navio 3
+
+    int linha_navio3  = 1;
+    int linha_final3  = 4;  
+    int coluna_navio3 = 1;
+    int coluna_final3 = 4; 
+
+    posicao = 0;
+
+    //Condição para verificar se o navio esta dentro dos limites do tabuleiro
+
+    if((linha_navio3 >= 0 && linha_navio3 <= 10) && (coluna_navio3 >= 0 && coluna_final3 <= 10)){ 
+        
+        for(int i = 0; i < 10; i++){ // Loop para percorrer todas as linhas
+
+            for(int j = 0; j < 10; j++){ // Loop para percorrer todas as colunas
+
+                if((i >= linha_navio3 && i < linha_final3) && (j >= coluna_navio3 && j < coluna_final3)){ // Condição para verificar a coordenadas do navio 3
+
+                     if(tabuleiro[i][j] != 0){ // Verifica se ja tem um navio na posição. Se tiver exibi a mensagem e encerra o programa.
+
+                        printf("Erro ! Navio encontrado na posição [%d][%d] do tabuleiro." , i , j);
+                        return 1;
+
+                    }else{
+
+                        if(i == j && posicao < 3){  //Condição para verificar se i e j são iguais e se o tamanho do navio esta sendo respeitado
+
+                            tabuleiro[i][j] = navio3[posicao]; // Copiando valor do vetor para a posição do navio no tabuleiro
+
+                            posicao++;
+                    
+                        }
+                     }
+
+                }
+               
+            }
+        }
+
+    }
+
+
+    // 7. Posicionando navio 4 na diagonal segundaria 
+    
+    posicao = 0;
+
+    int navio4[3] = {3, 3, 3};
+
+    //Declaração das variáveis que vão definir as coordenadas inicias do navio 4
+
+    int linha_navio4 = 6;
+    int final_linha4 = 9; 
+    int coluna_navio4 = 1;
+    int final_coluna4 = 4; 
+
+
+    //Condição para verificar se o navio esta dentro dos limites do tabuleiro
+
+    if((linha_navio4 >= 0 && final_linha4 <= 10) && (coluna_navio4 >= 0 && final_coluna4 <= 10)){ 
+
+        for(int i = 0; i < 10; i++){ // Loop para percorrer todas as linhas
+
+            for(int j = 0; j < 10; j++){ // Loop para percorrer todas as colunas
+                
+                if((i >= linha_navio4 && i < final_linha4) && (j >= coluna_navio4 && j < final_coluna4)){  // Condição para verificar a coordenadas do navio 4
+                
+                    if(tabuleiro[i][j] != 0) { // Verifica se ja tem um navio na posição. Se tiver exibi a mensagem e encerra o programa.
+                        
+                        printf("Erro ! Navio encontrado na posição [%d][%d] do tabuleiro" , i , j);
+                        return 1;
+
+                    }else{
+
+                        if((i + j == 9) && (posicao < 3)){ // Condição para verificar se a soma dos indices i e j são igual a 9 , e se o tamanho do navio esta sendo respeitado
+
+                            tabuleiro[i][j] = navio4[posicao]; // Copiando o valor do vetor para a posição do navio no tabuleiro
+                            posicao++;
+
+                        }
+                        
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    
+
+
+
 
 
     // 6.Exibindo tabuleiro
